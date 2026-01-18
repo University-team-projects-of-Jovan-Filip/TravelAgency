334551c302672161119c9ed8f98097b1d0e852ce
fili5rovic <fili5rovic@gmail.com>
2026-01-18 11:36:14 +0100

    added footer, main-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {MainMenuComponent} from "./main-menu/main-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
